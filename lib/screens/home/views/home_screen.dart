15a0f62a6af2bf9a328828b49e395c110fee79a2
Thor Fatih <thoriqpin@gmail.com>
2024-06-18 13:06:51 +0700

    article

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -2,63 +2,82 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fpppb2024/screens/CU_expenses_income/views/expense_income_page.dart';
 import 'package:fpppb2024/screens/home/views/main_screen.dart';
+import 'package:fpppb2024/screens/news/views/news_page.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _pages = [
+    MainScreen(),
+    NewsPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: MainScreen(),
+        child: _pages[_currentIndex],
       ),
       bottomNavigationBar: ClipRRect(
         borderRadius: const BorderRadius.vertical(
           top: Radius.circular(30),
         ),
         child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: _onItemTapped,
           showSelectedLabels: false,
           showUnselectedLabels: false,
           elevation: 3,
-          items: [
+          items: const [
             BottomNavigationBarItem(
               icon: Icon(CupertinoIcons.home),
-              label: 'Home'
+              label: 'Home',
             ),
             BottomNavigationBarItem(
               icon: Icon(CupertinoIcons.graph_square_fill),
-              label: 'Stats'
+              label: 'Stats',
             ),
           ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (BuildContext context) => ExpenseIncomePage()
-            )
+              builder: (BuildContext context) => ExpenseIncomePage(),
+            ),
           );
         },
         shape: const CircleBorder(),
         child: Container(
-            width: 60,
-            height: 60,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              gradient: LinearGradient(
-                colors: [
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: LinearGradient(
+              colors: [
                 Theme.of(context).colorScheme.primary,
                 Theme.of(context).colorScheme.secondary,
-                Theme.of(context).colorScheme.tertiary
-                ],
-              )
+                Theme.of(context).colorScheme.tertiary,
+              ],
             ),
-            child: Icon(CupertinoIcons.add)
+          ),
+          child: const Icon(CupertinoIcons.add),
         ),
-
       ),
     );
   }
